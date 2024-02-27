3eca96b63314bf33e7ecf7597921e24661e7145c
gabrieldiniz <gabriel.diniz@fengbrasil.com.br>
2024-02-27 00:41:31 -0300

    feat: add deployment for second pipeline

--- create_deployment.py
+++ create_deployment.py
@@ -9,3 +9,11 @@ if __name__ == "__main__":
         work_pool_name="my-managed-pool",
         cron="0 1 * * *",
     )
+
+    flow.from_source(
+        source="https://github.com/gabrieldinizsm/prefect.git",
+        entrypoint="sample_pipeline.py:dummy_flow",
+    ).deploy(
+        name="second-deployment",
+        work_pool_name="my-managed-pool"
+    )
--- sample_pipeline.py
+++ sample_pipeline.py
@@ -2,12 +2,24 @@ from prefect import flow, task
 
 
 @task(name="Doing something",
-      retries=2)
+      retries=2,
+      log_prints=True)
 def do_something():
     print("Doing something")
 
 
-@task("doing another thing",
-      retries=2)
+@task(name="doing another thing",
+      retries=2,
+      log_prints=True)
 def doing_another_thing():
     print("Doing another thing")
+
+
+@flow
+def dummy_flow():
+    do_something()
+    doing_another_thing()
+
+
+if __name__ == '__main__':
+    dummy_flow()
